c22bfadb23d4b96dd79fda865f1a5e1b585bbd86
Kaylah Andrews <kandr716@gmail.com>
2022-04-11 11:35:56 -0500

    add to method for gettinf contacts

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
+        Manager contactFiles = new Manager("contacts.txt", "data");
+
+//        ArrayList<FileManager> files = new ArrayList<>();
+//        files.add(new FileManager())
+
+        contactFiles.printLines();
+
 
     }
 
